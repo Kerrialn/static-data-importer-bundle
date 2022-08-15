d25f7bbac63190c441cb62e9247a1acd1b575d68
kez <kerrialbeckettnewham@gmail.com>
2022-08-15 21:33:18 +0200

    fix extension

---
+++ src/DependencyInjection/StaticDataImporterExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kerrialn\Bundle\StaticDataImporterBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class StaticDataImporterExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
--- src/StaticDataImporterBundle.php
+++ src/StaticDataImporterBundle.php
@@ -2,8 +2,28 @@
 
 namespace Kerrialn\Bundle\StaticDataImporterBundle;
 
+use Kerrialn\Bundle\StaticDataImporterBundle\DependencyInjection\StaticDataImporterExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class StaticDataImporterBundle extends AbstractBundle
 {
+    public function getContainerExtension() : ExtensionInterface
+    {
+        return new StaticDataImporterExtension();
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $configDir =__DIR__.'/../../../src/config/services.xml';
+
+        if(is_dir($configDir) === false){
+            return;
+        }
+
+        $container->import($configDir);
+    }
+
 }
