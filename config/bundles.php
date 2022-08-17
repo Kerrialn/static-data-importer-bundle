a8b7de55a065e1e26ca2fe291287daadd4ad8b04
kez <kerrialbeckettnewham@gmail.com>
2022-08-17 14:35:46 +0200

    fix composer issues

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Kerrialn\Bundle\StaticDataImporterBundle\StaticDataImporterBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
